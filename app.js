247547d3bc7b2a6ee1b422fe9e3338813e96f7b2
Maeda13 <igormassayukim@gmail.com>
2025-08-10 22:09:20 -0300

    Atualização

--- app.js
+++ app.js
@@ -1,11 +1,12 @@
 const express = require('express');
+const path = require('path');
 const app = express();
 const port = 3000;
 
-app.use(express.static('app/public'));
+app.use(express.static(path.join(__dirname, 'app/public')));
 
 app.set('view engine', 'ejs');
-app.set('views', './app/views');
+app.set('views', path.join(__dirname, 'app/views'));
 
 var rotas = require('./app/routes/router');
 app.use('/', rotas);
